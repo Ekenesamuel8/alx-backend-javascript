ea0a8df3ef7c3a38aada4b7f09f91b1d78108e31
ekenesamuel8 <chikwenduekene8@gmail.com>
2024-07-26 16:16:58 +0100

    promise

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,12 @@
+export default async function asyncUploadUser() {
+    let res = {};
+  
+    try {
+      const photo = await uploadPhoto();
+      const user = await createUser();
+      res = { photo, user };
+    } catch (err) {
+      res = { photo: null, user: null };
+    }
+    return res;
+  }
